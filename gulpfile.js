39e7b5ada92a448d9cb2531b427fbd7c34c572dd
Simon <Simon_Freelance@hotmail.com>
2018-03-02 19:35:49 -0500

    Fixed Sass watch in gulp task runner

--- gulpfile.js
+++ gulpfile.js
@@ -25,15 +25,6 @@ gulp.task('sass', function () {
         .pipe(browserSync.stream());
 });
 
-
-/*BELLO BOARD HOME*/
-gulp.task('sass', function () {
-    return gulp.src(['src/scss/bello-board-home/*.scss'])
-        .pipe(sass())
-        .pipe(gulp.dest("src/css/bello-board-home"))
-        .pipe(browserSync.stream());
-});
-
 // Move the javascript files into our /src/js folder
 gulp.task('js', function () {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
@@ -48,7 +39,7 @@ gulp.task('serve', ['sass'], function () {
         server: "./src"
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/*.scss', 'src/scss/bello-board-home/*.scss'], ['sass']);
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/**/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
